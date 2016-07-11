04a1738daa81dea4dda7d60df847b55fa530de22
kenpcjr <kennethcooke@me.com>
2016-07-11 10:13:48 -0400

    Done.

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -9,14 +9,166 @@
 import UIKit
 
 @UIApplicationMain
+
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let statesAndCapitals  = [
+            "Alabama":"Montgomery",
+            "Alaska":"Juneau",
+            "Arizona":"Phoenix",
+            "Arkansas":"Little Rock",
+            "California":"Sacramento",
+            "Colorado":"Denver",
+            "Connecticut":"Hartford",
+            "Delaware":"Dover",
+            "Florida":"Tallahassee",
+            "Georgia":"Atlanta",
+            "Hawaii":"Honolulu",
+            "Idaho":"Boise",
+            "Illinois":"Springfield",
+            "Indiana":"Indianapolis",
+            "Iowa":"Des Moines",
+            "Kansas":"Topeka",
+            "Kentucky":"Frankfort",
+            "Lousiana":"Baton Rouge",
+            "Maine":"Augusta",
+            "Maryland":"Annapolis",
+            "Massassachusetts":"Boston",
+            "Michigan":"Lansing",
+            "Minnesota":"Saint Paul",
+            "Mississippi":"Jackson",
+            "Missouri":"Jefferson City",
+            "Montana":"Helena",
+            "Nebraska":"Lincoln",
+            "Nevada":"Carson City",
+            "New Hampshire":"Concord",
+            "New Jersey":"Trenton",
+            "New Mexico":"Santa Fe",
+            "New York":"Albany",
+            "North Carolina":"Raleigh",
+            "North Dakota":"Bismarck",
+            "Ohio":"Columbus",
+            "Oklahoma":"Oklahoma City",
+            "Oregon":"Salem",
+            "Pennsylvania":"Harrisburg",
+            "Rhode Island":"Providence",
+            "South Carolina":"Columbia",
+            "South Dakota":"Pierre",
+            "Tennessee":"Nashville",
+            "Texas":"Austin",
+            "Utah":"Salt Lake City",
+            "Vermont":"Montpelier",
+            "Virgina":"Richmond",
+            "Washington":"Olympia",
+            "West Virginia":"Charleston",
+            "Wisconsin":"Madison",
+            "Wyoming":"Cheyenne"]
+        
+        print(solveTrivia(statesAndCapitals))
+        
+        
+        
+        
+        return true
+    }
     
     /*
      
      write your function here
      
      */
-
+    
+    let statesAndCapitals  = [
+        "Alabama":"Montgomery",
+        "Alaska":"Juneau",
+        "Arizona":"Phoenix",
+        "Arkansas":"Little Rock",
+        "California":"Sacramento",
+        "Colorado":"Denver",
+        "Connecticut":"Hartford",
+        "Delaware":"Dover",
+        "Florida":"Tallahassee",
+        "Georgia":"Atlanta",
+        "Hawaii":"Honolulu",
+        "Idaho":"Boise",
+        "Illinois":"Springfield",
+        "Indiana":"Indianapolis",
+        "Iowa":"Des Moines",
+        "Kansas":"Topeka",
+        "Kentucky":"Frankfort",
+        "Lousiana":"Baton Rouge",
+        "Maine":"Augusta",
+        "Maryland":"Annapolis",
+        "Massassachusetts":"Boston",
+        "Michigan":"Lansing",
+        "Minnesota":"Saint Paul",
+        "Mississippi":"Jackson",
+        "Missouri":"Jefferson City",
+        "Montana":"Helena",
+        "Nebraska":"Lincoln",
+        "Nevada":"Carson City",
+        "New Hampshire":"Concord",
+        "New Jersey":"Trenton",
+        "New Mexico":"Santa Fe",
+        "New York":"Albany",
+        "North Carolina":"Raleigh",
+        "North Dakota":"Bismarck",
+        "Ohio":"Columbus",
+        "Oklahoma":"Oklahoma City",
+        "Oregon":"Salem",
+        "Pennsylvania":"Harrisburg",
+        "Rhode Island":"Providence",
+        "South Carolina":"Columbia",
+        "South Dakota":"Pierre",
+        "Tennessee":"Nashville",
+        "Texas":"Austin",
+        "Utah":"Salt Lake City",
+        "Vermont":"Montpelier",
+        "Virgina":"Richmond",
+        "Washington":"Olympia",
+        "West Virginia":"Charleston",
+        "Wisconsin":"Madison",
+        "Wyoming":"Cheyenne"]
+    
+    
+    
+    func solveTrivia(dictionary: [String : String]) -> String {
+        
+        var testedState = ""
+        var containsSameCharacter = true
+        for (state, capital) in dictionary {
+            
+            testedState = state
+            
+            let lowercaseState = state.lowercaseString
+            let lowercaseCapital = capital.lowercaseString
+            
+            let stateAsCharacters = lowercaseState.characters
+            let capitalsAsCharacters = lowercaseCapital.characters
+            
+            for character in stateAsCharacters {
+                
+                if capitalsAsCharacters.contains(character) {
+                    containsSameCharacter = true
+                    break
+                }else{
+                    containsSameCharacter = false
+                }
+                
+            }
+            if containsSameCharacter == false {
+                return testedState
+            }
+        }
+        return "There's no such combination."
+    }
+    
+    
 }
 
